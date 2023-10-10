209ef0ffb51c4ee228371aa0601190931c74ccc1
Pedro Jorge das Neves Pinto Vieira <pedro10geral@gmail.com>
2023-10-10 01:32:45 +0100

    calculateSpan implemented

--- TP 3/Tests/funListStackQueueProblem.cpp
+++ TP 3/Tests/funListStackQueueProblem.cpp
@@ -54,9 +54,10 @@ vector<string> FunListStackQueueProblem::binaryNumbers(int n) {
     binaries.emplace("1");
     while (n--) {
         res.push_back(binaries.front());
+        //In each iteration we add a 0 and then a 1 to the first element of the queue, generating the next binaries
         binaries.push(binaries.front() + "0");
         binaries.push(binaries.front() + "1");
-        binaries.pop();
+        binaries.pop(); //To go to the next element
     }
     return res;
 }
@@ -68,6 +69,15 @@ vector<string> FunListStackQueueProblem::binaryNumbers(int n) {
 //TODO
 vector<int> FunListStackQueueProblem::calculateSpan(vector<int> prices) {
     vector<int> res;
+    stack<int> closet_day_index; //This stack will register the index of temporary closest days, with the last one on the top
+    for (int i = 0; i < prices.size(); i++) {
+        while (!closet_day_index.empty() && prices[i] > prices[closet_day_index.top()]) {
+            closet_day_index.pop(); //We remove the top of the stack until the stack is empty or
+                                    //we find the closest day greater than prices[i]
+        }
+        closet_day_index.empty() ? res.push_back(i + 1) : res.push_back(i - closet_day_index.top()); //We calculate the length of the referred interval
+        closet_day_index.push(i); //We have a new closest day!
+    }
     return res;
 }
 
--- TP 3/cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/Desktop/FEUP/AED/TP 3/lib/googletest/googletest
-# Build directory: C:/Users/User/Desktop/FEUP/AED/TP 3/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

15dba81f65de6cdbfe39fea2be567de698f35bcb
Pedro Jorge das Neves Pinto Vieira <pedro10geral@gmail.com>
2023-10-23 14:27:33 +0100

    gitignore deleted

---
+++ AED_project_1/cmake-build-debug/CMakeFiles/projeto_1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projeto_1.dir/schedule/src/SetStudents.cpp.obj"
+  "CMakeFiles/projeto_1.dir/schedule/src/SetStudents.cpp.obj.d"
+  "CMakeFiles/projeto_1.dir/schedule/src/Student.cpp.obj"
+  "CMakeFiles/projeto_1.dir/schedule/src/Student.cpp.obj.d"
+  "CMakeFiles/projeto_1.dir/schedule/src/main.cpp.obj"
+  "CMakeFiles/projeto_1.dir/schedule/src/main.cpp.obj.d"
+  "CMakeFiles/projeto_1.dir/schedule/src/pairUcClasses.cpp.obj"
+  "CMakeFiles/projeto_1.dir/schedule/src/pairUcClasses.cpp.obj.d"
+  "CMakeFiles/projeto_1.dir/schedule/src/ucClasses.cpp.obj"
+  "CMakeFiles/projeto_1.dir/schedule/src/ucClasses.cpp.obj.d"
+  "libprojeto_1.dll.a"
+  "projeto_1.exe"
+  "projeto_1.exe.manifest"
+  "projeto_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projeto_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AED_project_1/cmake-build-debug/CMakeFiles/projeto_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for projeto_1.
+# This may be replaced when dependencies are built.
